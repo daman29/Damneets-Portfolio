f7f2b4db00a0f559e2ada8c22abb33767b16d05d
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-07-04 16:59:07 +0930

    updated portfolio with latest project

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,18 @@ const modal = document.querySelector('#myModal')
 const span = document.getElementsByClassName("close")[0];
 const phoneBtn = document.querySelector('#phone')
 
-projectsContainer.addEventListener('mouseover', function (e) {
+// projectsContainer.addEventListener('mouseover', function (e) {
     
-    if (e.target.id === 'project1') {
-        e.target.src = 'assets/images/project1gif.gif'
-    }
-})
+//     if (e.target.id === 'project1') {
+//         e.target.src = 'assets/images/project1gif.gif'
+//     }
+// })
 
-projectsContainer.addEventListener('mouseout', function(e){
-    if (e.target.id === 'project1') {
-        e.target.src = 'assets/images/easy-date.png'
-    }
-})
+// projectsContainer.addEventListener('mouseout', function(e){
+//     if (e.target.id === 'project1') {
+//         e.target.src = 'assets/images/easy-date.png'
+//     }
+// })
 
 phoneBtn.addEventListener('click',function(e){
     e.preventDefault();
